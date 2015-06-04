ec9b0d704046cf19b5876e6fde3c605874f1bf6d
YuxuanHe <yuxuanhe2016@u.northwestern.edu>
2015-06-04 11:46:33 -0500

    add test file

--- kademlia/src/kademlia/kademlia_test.go
+++ kademlia/src/kademlia/kademlia_test.go
@@ -209,61 +209,89 @@ func StringToIpPort(laddr string) (ip net.IP, port uint16, err error){
 //   t.Logf(result)
 // }
 
-func TestIterativeFindValue(t *testing.T) {
+// func TestIterativeFindValue(t *testing.T) {
+//   // using line structure;
+//   /*
+//     A->B->C->D->E->F->G
+//   */
+//   line_node := make([]*Kademlia, 80)
+//   line_node[0] = NewKademlia("localhost:8030")
+//   for i := 1; i < 20; i++ {
+//       address := "localhost:"+strconv.Itoa(8031+i)
+//       line_node[i] = NewKademlia(address)
+//       host_number, port_number, _ := StringToIpPort(address)
+//       line_node[i-1].DoPing(host_number, port_number)
+//   }
+//   contact, err := line_node[18].FindContact(line_node[19].NodeID)
+//   if err != nil {
+//       t.Error("Instance 29's contact not found in Instance 28's contact list")
+//       return
+//   }
+//   Key := NewRandomID()
+//   Value := []byte("Hello world")
+//
+//   // given the right key
+//   result := line_node[19].DoStore(contact,Key, Value)
+//   if p:= strings.Index(result,"OK");p!=0 {
+//     t.Error("Can't store value at the end of line")
+//   }
+//   result = line_node[0].DoIterativeFindValue(Key)
+//   if p:= strings.Index(result,"OK");p!=0 {
+//     t.Error("Can't iterative find value")
+//   }
+//   //t.Logf(result)
+//   //test if the key was wrong
+//   Key_wrong := NewRandomID()
+//   result = line_node[0].DoIterativeFindValue(Key_wrong)
+//   if p:= strings.Index(result,"OK");p==0 {
+//     t.Error("The key was wrong, it should not return OK")
+//   }
+//   t.Logf(result)
+//
+//   //if there is only two node in this network
+//   first_node := NewKademlia("localhost:8130")
+//   second_node := NewKademlia("localhost:8131")
+//   address_second := "localhost:"+strconv.Itoa(8131)
+//   host_number_second, port_number_second, _ := StringToIpPort(address_second)
+//   first_node.DoPing(host_number_second, port_number_second)
+//   contact_second_node, err := first_node.FindContact(second_node.NodeID)
+//   if err != nil {
+//       t.Error("the first node and the second node are not connected")
+//       return
+//   }
+//   first_node.DoStore(contact_second_node, Key, Value)
+//   result = first_node.DoIterativeFindValue(Key)
+//   if p:= strings.Index(result,"OK");p!=0 {
+//     t.Error("Can't iterative find value")
+//   }
+//   //t.Logf(result)
+//
+// }
+
+
+func TestVanish(t *testing.T) {
   // using line structure;
   /*
     A->B->C->D->E->F->G
   */
-  line_node := make([]*Kademlia, 80)
-  line_node[0] = NewKademlia("localhost:8030")
-  for i := 1; i < 20; i++ {
-      address := "localhost:"+strconv.Itoa(8031+i)
+  line_node := make([]*Kademlia, 30)
+  line_node[0] = NewKademlia("localhost:7990")
+  for i := 1; i < 30; i++ {
+      address := "localhost:"+strconv.Itoa(7991+i)
       line_node[i] = NewKademlia(address)
       host_number, port_number, _ := StringToIpPort(address)
       line_node[i-1].DoPing(host_number, port_number)
   }
-  contact, err := line_node[18].FindContact(line_node[19].NodeID)
-  if err != nil {
-      t.Error("Instance 29's contact not found in Instance 28's contact list")
-      return
-  }
-  Key := NewRandomID()
-  Value := []byte("Hello world")
 
-  // given the right key
-  result := line_node[19].DoStore(contact,Key, Value)
-  if p:= strings.Index(result,"OK");p!=0 {
-    t.Error("Can't store value at the end of line")
-  }
-  result = line_node[0].DoIterativeFindValue(Key)
-  if p:= strings.Index(result,"OK");p!=0 {
-    t.Error("Can't iterative find value")
-  }
-  //t.Logf(result)
-  //test if the key was wrong
-  Key_wrong := NewRandomID()
-  result = line_node[0].DoIterativeFindValue(Key_wrong)
-  if p:= strings.Index(result,"OK");p==0 {
-    t.Error("The key was wrong, it should not return OK")
-  }
+  //
+  VDOID := NewRandomID()
+  Data := []byte("Hello")
+  N := byte(4)
+  T := byte(2)
+
+  result := line_node[0].Vanish(VDOID,Value)
+  // if p:= strings.Index(result,"OK");p!=0 {
+  //   t.Error("Can't store value")
+  // }
   t.Logf(result)
-
-  //if there is only two node in this network
-  first_node := NewKademlia("localhost:8130")
-  second_node := NewKademlia("localhost:8131")
-  address_second := "localhost:"+strconv.Itoa(8131)
-  host_number_second, port_number_second, _ := StringToIpPort(address_second)
-  first_node.DoPing(host_number_second, port_number_second)
-  contact_second_node, err := first_node.FindContact(second_node.NodeID)
-  if err != nil {
-      t.Error("the first node and the second node are not connected")
-      return
-  }
-  first_node.DoStore(contact_second_node, Key, Value)
-  result = first_node.DoIterativeFindValue(Key)
-  if p:= strings.Index(result,"OK");p!=0 {
-    t.Error("Can't iterative find value")
-  }
-  //t.Logf(result)
-
 }
